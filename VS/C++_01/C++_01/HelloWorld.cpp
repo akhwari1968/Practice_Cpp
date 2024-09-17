c0797bf62296de1de64071247add383c126b0fd0
king176769 <2630176769@qq.com>
2024-09-17 22:25:23 +0800

    test

---
+++ VS/C++_01/C++_01/HelloWorld.cpp
@@ -0,0 +1,19 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	/*short num = 10;
+	cout << sizeof(int) << endl;
+
+	system("pause");*/
+	/*×¢ÊÍ*/
+
+	int age = 0;
+	cin >> age;
+	cout << age << endl;
+
+	return 0;
+}
