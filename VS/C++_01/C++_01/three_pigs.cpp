3fe2f461b30f4dbb4da73f801b5adcc79152d7d5
king176769 <2630176769@qq.com>
2024-09-18 21:33:30 +0800

    9.18

---
+++ VS/C++_01/C++_01/three_pigs.cpp
@@ -0,0 +1,27 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int one = 0;
+	int two = 0;
+	int three = 0;
+
+	cout << "请输入1的：";
+	cin >> one;
+	cout << "请输入2的：";
+	cin >> two;
+	cout << "请输入3的：";
+	cin >> three;
+
+	if (one > two && one > three)
+	cout << "最重的是：one" << endl;
+	else if (one < two && two > three)
+	cout << "最重的是：two" << endl;
+	else if (one < three && two < three)
+	cout << "最重的是：three" << endl;
+
+	system("pause");
+	return 0;
+}
