fb345b2e7d025103ba2b06a379e7acdbf28da94e
king176769 <2630176769@qq.com>
2024-09-22 20:46:50 +0800

    9.22

--- VS/C++_01/C++_01/three_pigs.cpp
+++ VS/C++_01/C++_01/three_pigs.cpp
@@ -1,89 +1,130 @@
-#define  _CRT_SECURE_NO_WARNINGS 1
-#include <iostream>
-using namespace std;
-
-int main()
-{
-	/*int one = 0;
-	int two = 0;
-	int three = 0;
-
-	cout << "请输入1的：";
-	cin >> one;
-	cout << "请输入2的：";
-	cin >> two;
-	cout << "请输入3的：";
-	cin >> three;
-
-	if (one > two && one > three)
-	cout << "最重的是：one" << endl;
-	else if (one < two && two > three)
-	cout << "最重的是：two" << endl;
-	else if (one < three && two < three)
-	cout << "最重的是：three" << endl;*/
-
-	/*int a = 0;
-	int b = 0;
-
-	while (cin >> a >> b)
-	{
-		cout << (a + b) << endl;
-	}*/
-
-	/*float c = 0;
-	float c1 = 0;
-
-	cin >> c;
-
-	c1 = 5.0 * (c - 32.0) / 9.0;
-	printf("c=%.2f\n", c1);*/
-	/*srand((unsigned int)time(NULL));
-
-	int num = rand() % 100 + 1;
-	cout << num << endl;*/
-	
-	/*int num = 0;
-	int i = 100;
-	do
-	{
-		num = (i / 100) * (i / 100) * (i / 100);
-		num += (i / 10 % 10) * (i / 10 % 10) * (i / 10 % 10);
-		num += (i % 10) * (i % 10) * (i % 10);
-
-		if (num == i)
-		{
-			cout << num << endl;
-		}
-		i++;
-
-	} while (i < 1000);*/
-
-	////i/*nt i = 0;
-	//int j = 0;
-	//int col = 0;
-	//int row = 0;
-	//cin >> col;
-	//cin >> row;
-	//for (i = 0; i < col; i++)
-	//{
-	//	for (j = 0; j < row; j++)
-	//	{
-	//		cout << "* ";
-	//	}
-	//	cout << endl;
-	//}*/
-
-	int i = 1;
-	int j = 1;
-	for (i = 1; i <= 9; i++)
-	{
-		for (j = 1; j <= i; j++)
-		{
-			cout << j << "*" << i << "=" << (j * i) << "\t";
-		}
-		cout << endl;
-	}
-
-	system("pause");
-	return 0;
-}
+//#define  _CRT_SECURE_NO_WARNINGS 1
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	/*int one = 0;
+//	int two = 0;
+//	int three = 0;
+//
+//	cout << "请输入1的：";
+//	cin >> one;
+//	cout << "请输入2的：";
+//	cin >> two;
+//	cout << "请输入3的：";
+//	cin >> three;
+//
+//	if (one > two && one > three)
+//	cout << "最重的是：one" << endl;
+//	else if (one < two && two > three)
+//	cout << "最重的是：two" << endl;
+//	else if (one < three && two < three)
+//	cout << "最重的是：three" << endl;*/
+//
+//	/*int a = 0;
+//	int b = 0;
+//
+//	while (cin >> a >> b)
+//	{
+//		cout << (a + b) << endl;
+//	}*/
+//
+//	/*float c = 0;
+//	float c1 = 0;
+//
+//	cin >> c;
+//
+//	c1 = 5.0 * (c - 32.0) / 9.0;
+//	printf("c=%.2f\n", c1);*/
+//	/*srand((unsigned int)time(NULL));
+//
+//	int num = rand() % 100 + 1;
+//	cout << num << endl;*/
+//	
+//	/*int num = 0;
+//	int i = 100;
+//	do
+//	{
+//		num = (i / 100) * (i / 100) * (i / 100);
+//		num += (i / 10 % 10) * (i / 10 % 10) * (i / 10 % 10);
+//		num += (i % 10) * (i % 10) * (i % 10);
+//
+//		if (num == i)
+//		{
+//			cout << num << endl;
+//		}
+//		i++;
+//
+//	} while (i < 1000);*/
+//
+//	////i/*nt i = 0;
+//	//int j = 0;
+//	//int col = 0;
+//	//int row = 0;
+//	//cin >> col;
+//	//cin >> row;
+//	//for (i = 0; i < col; i++)
+//	//{
+//	//	for (j = 0; j < row; j++)
+//	//	{
+//	//		cout << "* ";
+//	//	}
+//	//	cout << endl;
+//	//}*/
+//
+//	/*int i = 1;
+//	int j = 1;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			cout << j << "*" << i << "=" << (j * i) << "\t";
+//		}
+//		cout << endl;
+//	}*/
+//
+//	int arr[5];
+//	int i = 0;
+//	int max = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		cin >> arr[i];
+//		if (max < arr[i])
+//		{
+//			max = arr[i];
+//		}
+//	}
+//
+//	cout << max << endl;
+//
+//	/*int arr[5];
+//	int i = 0;
+//	int left = 0;
+//	int right = 4;
+//	for (i = 0; i < 5; i++)
+//	{
+//		cin >> arr[i];
+//	}*/
+//
+//	/*int tmp = 0;
+//	while (left != right)
+//	{
+//		tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//
+//	for (i = 0; i < 5; i++)
+//	{
+//		cout << arr[i] << " ";
+//	}
+//	cout << endl;*/
+//
+//
+//
+//	system("pause");
+//	return 0;
+//}
