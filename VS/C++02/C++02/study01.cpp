02bbcdf503814385fe4cfd5251494a4e5024a0db
king176769 <2630176769@qq.com>
2024-09-23 23:14:45 +0800

    9.23

--- VS/C++02/C++02/study01.cpp
+++ VS/C++02/C++02/study01.cpp
@@ -4,8 +4,29 @@ using namespace std;
 
 int main()
 {
-	
+	int c = 0;
+	cin >> c;
 
-	system("pasue");
+	if (c >= 90)
+	{
+		cout << "A" << endl;
+	}
+	else if (c >= 80)
+	{
+		cout << "B" << endl;
+	}
+	else if (c >= 70)
+	{
+		cout << "C" << endl;
+	}
+	else if (c >= 60)
+	{
+		cout << "D" << endl;
+	}
+	else
+	{
+		cout << "E" << endl;
+	}
+	system("pause");
 	return 0;
 }
