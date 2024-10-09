7935cbe642943332eb3e61936bd40ccd7d764637
king176769 <2630176769@qq.com>
2024-10-09 23:06:52 +0800

    10.09

--- VS/C++02/C++02/study02.cpp
+++ VS/C++02/C++02/study02.cpp
@@ -1,17 +1,37 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
 #include <string>
-#define Swap(num1,num2) (temp = num1,num1 = num2,num2 = temp)
+//#define Swap(num1,num2) (temp = num1,num1 = num2,num2 = temp)
 using namespace std;
 
-int temp = 0;
+//int temp = 0;
 int main()
 {
-	int val1 = 0;
+	/*int val1 = 0;
 	int val2 = 0;
 	cin >> val1 >> val2;
 	Swap(val1, val2);
-	cout << val1 << " " << val2 << endl;
+	cout << val1 << " " << val2 << endl;*/
+
+	string str;
+	getline(cin, str);
+	int i = 0;
+	for (i = 0; i < str.length(); i++)
+	{
+		if (isalpha(str[i]))
+		{
+			if (str[i] == 122)
+			{
+				str[i] = 'a';
+			}
+			else
+			{
+				str[i] += 1;
+			}
+		}
+	}
+
+	cout << str << endl;
 
 	system("pause");
 	return 0;
