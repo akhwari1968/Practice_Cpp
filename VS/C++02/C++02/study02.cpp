33107c4d79d0e669c0e50a6bdae3feb7a352ff36
king176769 <2630176769@qq.com>
2024-10-13 22:29:50 +0800

    10.13

--- VS/C++02/C++02/study02.cpp
+++ VS/C++02/C++02/study02.cpp
@@ -4,6 +4,21 @@
 //#define Swap(num1,num2) (temp = num1,num1 = num2,num2 = temp)
 using namespace std;
 
+int isprime(int m)
+{
+	int i = 0;
+	int t = 1;
+	for (i = 2; i < m; i++)
+	{
+		if (!(m % i))
+		{
+			t = 0;
+			return t;
+		}
+	}
+	return t;
+}
+
 //int temp = 0;
 int main()
 {
@@ -33,7 +48,7 @@ int main()
 
 	cout << str << endl;*/
 
-	string str;
+	/*string str;
 	getline(cin, str);
 	int len = str.length();
 	int i = 0;
@@ -41,7 +56,19 @@ int main()
 	{
 		printf("%c", str[i]);
 	}
-	cout << endl;
+	cout << endl;*/
+
+	int m = 0, n = 0,sum = 0;
+	cin >> m >> n;
+	for (m = m; m <= n; m++)
+	{
+		if (isprime(m))
+		{
+			sum += m;
+		}
+	}
+
+	cout << sum << endl;
 
 	system("pause");
 	return 0;
