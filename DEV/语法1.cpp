f75c75be1d8b4f3a2a3c6cdbc12c32fa293e961c
king176769 <2630176769@qq.com>
2024-10-18 23:36:01 +0800

    10.18

--- DEV/语法1.cpp
+++ DEV/语法1.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
 	//cout << 3 << endl;
+	
+	string str;
+	cin >> str;
+	int i = 0;
+	
+	for (i = 0;i < str.length();i++)
+  {
+    if (str[i] == 'a')
+    {
+      str[i] = 'A';
+    }
+    else if (str[i] == 'e')
+    {
+      str[i] = 'E';
+    }
+    else if (str[i] == 'i')
+    {
+      str[i] = 'I';
+    }
+    else if (str[i] == 'o')
+    {
+      str[i] = 'O';
+    }
+    else if (str[i] == 'u')
+    {
+      str[i] = 'U';
+    }
+  }
+
+  cout << str << endl;
 	
 	return 0;
 }
