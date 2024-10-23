19e2e0b2de9b05db7a15824813eb200cf0c7faa5
king176769 <2630176769@qq.com>
2024-10-23 23:53:53 +0800

    10.23

---
+++ DEV/艺术与篮球.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+using namespace std;
+
+int isrun(int year)
+{
+  if (((year % 4 == 0) && (year % 100 != 0)) || year % 400 == 0)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int number(int x)
+{
+  if (x == 0)
+  return 13;
+  else if (x == 1)
+  return 1;
+  else if (x == 2)
+  return 2;
+  else if (x == 3)
+  return 3;
+  else if (x == 4)
+  return 5;
+  else if (x == 5)
+  return 4;
+  else if (x == 6)
+  return 4;
+  else if (x == 7)
+  return 2;
+  else if (x == 8)
+  return 2;
+  else if (x == 9)
+  return 2;
+}
+
+int caculation_year(int year)
+{
+  return number(year / 1000) + number(year / 100 % 10) + (year / 10 % 10) + number(year % 10);
+}
+
+int caculation_month(int month)
+{
+  if (month < 10)
+  {
+    return 13 + number(month);
+  }
+  else
+  {
+    return number(month / 10) + number(month % 10);
+  }
+}
+
+int caculation_day(int day)
+{
+  if (day < 10)
+  {
+    return 13 + number(day);
+  }
+  else
+  {
+    return number(day / 10) + number(day % 10);
+  }
+}
+
+int main()
+{
+  int year = 2000,month = 1,day = 1;
+  int sum = 0;
+
+  for (year = 2000;year <= 2024;year++)
+  {
+      for (int month = 1;month <= 12;month++)
+      {
+        if (month == 2)//二月处理
+        {
+          if (isrun(year))
+          {
+            for (day = 1;day < 30;day++)
+            {
+              if ((caculation_month(month) + caculation_month(month) + caculation_day(day)) > 50)
+              {
+                sum += 1;
+              }
+            }
+          }
+          else
+          {
+            for (day = 1;day < 29;day++)
+            {
+              if ((caculation_month(month) + caculation_month(month) + caculation_day(day)) > 50)
+              {
+                sum += 1;
+              }
+            }
+          }
+        }
+        else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 ||month == 10 || month == 12)
+        {
+          for (day = 1;day < 32;day++)
+          {
+            if ((caculation_month(month) + caculation_month(month) + caculation_day(day)) > 50)
+              {
+                sum += 1;
+              }
+          }
+        }
+        else
+        {
+          for (day = 1;day < 31;day++)
+          {
+            if ((caculation_month(month) + caculation_month(month) + caculation_day(day)) > 50)
+              {
+                sum += 1;
+              }
+          }
+        }
+      }
+    }
+  }
+
+  cout << num;
+
+  return 0;
+}
