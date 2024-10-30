074f73eeaa6226ee78d8a53468d98442165075c2
king176769 <2630176769@qq.com>
2024-10-30 23:35:15 +0800

    10.31

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,67 +1,25 @@
 #include <iostream>
-#include <string>
-
-int findr(char* str, char* sub)
-{
-    int i = 0;
-    int j = 0;
-    int strLen = 0;
-    int subLen = 0;
-    if (str == NULL || sub == NULL) 
-    {
-        return -1;
-    }
-
-    while (str[strLen] != '\0') 
-    {
-        strLen++;
-    }
-
-    while (sub[subLen] != '\0') 
-    {
-        subLen++;
-    }
-
-    if (subLen > strLen) 
-    {
-        return -1;
-    }
-
-    for (i = strLen - subLen; i >= 0; i--) 
-    {
-        for (j = 0; j < subLen; j++) 
-        {
-            if (str[i + j] != sub[j]) 
-            {
-                break;
-            }
-        }
-        if (j == subLen) 
-        {
-            return i;
-        }
-    }
-    return -1;
-
-}
-
 using namespace std;
+
 int main()
 {
-  string str;
-  cin >> str;
-  int y = 0,f = 0,i = 0;
-  for (i = 0;i < str.length();i++)
+  int a = -3,b = -3,c = -3,count = 0;;
+  for (a = -3;a < 4;a++)
   {
-    if (str[i] == 'a' ||str[i] == 'e' ||str[i] == 'i' ||str[i] == 'o' ||str[i] == 'u')
+    for (b = -3;b < 4;b++)
     {
-      y++;
-    }
-    else
-    {
-      f++;
+      for (c = -3;c < 4;c++)
+      {
+        int sum1 = a * a * a + b * b * b + c * c * c;
+        int sum2 = 3 * a * b * c;
+        if (sum1 == sum2)
+        {
+          count++;
+        }
+      }
     }
   }
-  cout << y << endl << f;
+
+  cout << count;
   return 0;
 }
