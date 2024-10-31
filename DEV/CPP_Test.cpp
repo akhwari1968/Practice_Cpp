3f0b1411bb79cf64824759adfa29e1ca822a7802
king176769 <2630176769@qq.com>
2024-10-31 23:20:46 +0800

    10.31

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -3,23 +3,6 @@ using namespace std;
 
 int main()
 {
-  int a = -3,b = -3,c = -3,count = 0;;
-  for (a = -3;a < 4;a++)
-  {
-    for (b = -3;b < 4;b++)
-    {
-      for (c = -3;c < 4;c++)
-      {
-        int sum1 = a * a * a + b * b * b + c * c * c;
-        int sum2 = 3 * a * b * c;
-        if (sum1 == sum2)
-        {
-          count++;
-        }
-      }
-    }
-  }
-
-  cout << count;
-  return 0;
+	//眼睛疼，休息一晚
+	return 0;
 }
