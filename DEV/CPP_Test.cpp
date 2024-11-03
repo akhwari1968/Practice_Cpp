86ee3944321f054f01f87963435016551be29113
king176769 <2630176769@qq.com>
2024-11-03 23:52:33 +0800

    11.3

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,58 +1,30 @@
 #include <iostream>
 using namespace std;
-
-int isleapyear(int year)
-{
-  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-  {
-    return 1;
-  }
-  else
-  {
-    return 0;
-  }
-}
-
 int main()
 {
-  int days[13] = {0,31,0,31,30,31,30,31,31,30,31,30,31};
-  int number[10] = {13,1,2,3,5,4,4,2,2,2};
-  int year = 2000,month = 1,day = 1,num = 0,chinese = 0,math = 0;
+  int n = 0,i = 1,ok = 0;
+  int arr[200001];
 
-  for (year = 2000;year <= 2024;year++)
+  cin >> n;
+  if (n == 1)
   {
-    if (isleapyear(year))
-    {
-      days[2] = 29;
-    }
-    else
-    {
-      days[2] = 28;
-    }
-    for (month = 1;month <= 12;month++)
-    {
-      for (day = 1;day <= days[month];day++)
-      {
-        math = (year * 10000) + (month * 100) + day;
+    return ok;
+  }
+  while (i <= n)
+  {
+    cin >> arr[i++];
+  }
 
-        while (math)
-        {
-          num += number[math % 10];
-          math /= 10;
-          if (num > 50)
-          {
-            chinese++;
-            break;
-          }
-        }
-        num = 0;
-
-        if (year == 2024 && month == 4 && day == 13)
-        {
-          cout << chinese;
-        }
-      }
+  i = 0;
+  while (1)
+  {
+    i += 2;
+    for ()
+    for (int j = i;j > 0;j--)
+    {
+        
     }
   }
+
   return 0;
 }
