fd40797c944799832eeb7ebd105cb608e82351c5
king176769 <2630176769@qq.com>
2024-11-07 23:43:06 +0800

    11.7

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -2,22 +2,6 @@
 using namespace std;
 int main()
 {
-  long long t = 0,x = 0,y = 0,sum = 0;
-  cin >> t;
-
-  while(t--)
-  {
-    cin >> x >> y;
-    sum = x - y;
-    if (sum <= 0)
-    {
-      cout << 0 << endl;
-    }
-    else
-    {
-      cout << sum << endl;
-    }
-
-  }
+  "忙事"
   return 0;
 }
