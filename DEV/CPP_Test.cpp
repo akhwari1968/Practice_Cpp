9ea10032ca598e526be4177234725fe0b2be9a8e
king176769 <2630176769@qq.com>
2024-11-10 23:34:02 +0800

    11.10

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,41 +1,16 @@
 #include <iostream>
 using namespace std;
-
-int isleapyear(int year)
-{
-  if ((year % 100 != 0 && year % 4 == 0) || year % 400 == 0)
-  return 1;
-  else
-  return 0;
-}
-
 int main()
 {
-  int m_day [13] = {0,31,0,31,30,31,30,31,31,30,31,30,31};
-  int year = 0,month = 0,day = 0,count = 0,week = 1;
-
-  for (year = 1900;year <= 2000;year++)
+  int T = 0,N = 0;
+  cin >> T;
+  for (T;T > 0;T--)
   {
-    if (isleapyear(year))
-    m_day[2] = 29;
+    cin >> N;
+    if (N % 3 == 0)
+    cout << "YES" << endl;
     else
-    m_day[2] = 28;
-    for (month = 1;month <= 12;month++)
-    {
-      for (day = 1;day <= m_day[month];day++)
-      {
-        if (year != 1900 && day == 1 && week == 7)
-        count += 1;
-
-        if (week < 7)
-        week += 1;
-        else
-        week = 1;
-      }
-    }
+    cout << "NO" << endl;
   }
-
-  cout << count;
-
   return 0;
 }
