3896468374195f1064fc181258b45830f03c232a
king176769 <2630176769@qq.com>
2024-11-15 23:39:08 +0800

    11.15

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,15 +1,1 @@
-#include <iostream>
-#include <string>
-using namespace std;
-int main()
-{
-  string str;
-  cin >> str;
-  if (str[str.length() - 1] == '0')
-  str[str.length() - 1] = '1';
-  else
-  str[str.length() - 1] = '0';
-  cout << str;
-
-  return 0;
-}
+//休息
