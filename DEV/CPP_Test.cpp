8a6d0fd94df666fbf3e685ccc639dde971161508
king176769 <2630176769@qq.com>
2024-12-25 21:39:24 +0800

    12.25

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-using namespace
+using namespace std;
 
 int main()
 {
-	
+	int t,x,y;
+	cin >> t;
+	while (t--)
+	{
+		cin >> x >> y;
+		if (x * 5 >= y)
+		{
+			cout << "YES" << endl;
+		}
+		else
+		{
+			cout << "NO" << endl;
+		}
+	}
 	
 	return 0;
 }
