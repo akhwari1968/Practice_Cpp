6041ca323601faf6cbe43cb10620b244d444dde3
king176769 <2630176769@qq.com>
2024-12-26 20:59:59 +0800

    12.26

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-	int t,x,y;
-	cin >> t;
-	while (t--)
-	{
-		cin >> x >> y;
-		if (x * 5 >= y)
-		{
-			cout << "YES" << endl;
-		}
-		else
-		{
-			cout << "NO" << endl;
-		}
-	}
-	
-	return 0;
+  int n,d,t,sum;
+  int i = 0,j = 0;
+
+  cin >> n >> d;
+  while (n--)
+  {
+    cin >> t;
+    if (t >= 80 || t <= 9)
+    {
+      //危险
+      i++;
+    }
+    else
+    {
+      //不危险
+      j++;
+    }
+  }
+
+  sum = (i / d) + (j / d);
+  if (i - i / d != 0)
+  sum += 1;
+  if (j - j / d != 0)
+  sum += 1;
+  
+  cout << sum;
+
+  return 0;
 }
