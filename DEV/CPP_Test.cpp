1c09dfeb883155e91df6b31b037572273a5ed695
king176769 <2630176769@qq.com>
2024-12-28 22:19:11 +0800

    12.28

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -2,20 +2,7 @@
 using namespace std;
 int main()
 {
-  int t;
-  long long x,y;
-
-  cin >> t;
-  while (t--)
-  {
-    cin >> x >> y;
-    if (x > y)
-    cout << "BRIDGE" << endl;
-    if (x < y)
-    cout << "YI" << endl;
-    if (x == y)
-    cout << "ANY" << endl;
-  }
+  
 
   return 0;
 }
