7f88e70e0dadebb3ed6bd23f5d3d51478c301083
king176769 <2630176769@qq.com>
2024-12-29 20:17:12 +0800

    12.29

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -2,7 +2,21 @@
 using namespace std;
 int main()
 {
-  
+  int t,x,y,sum;
+  cin >> t;
+
+  while (t--)
+  {
+    cin >> x >> y;
+    if (y <= x)
+    {
+      sum = (x / y) + (x % y);
+    }
+    else
+    sum = x;
+
+    cout << sum << endl;
+  }
 
   return 0;
 }
