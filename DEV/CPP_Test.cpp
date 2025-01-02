7827f0992de6352fe105fa7bbdad00020ce70811
king176769 <2630176769@qq.com>
2025-01-02 21:17:14 +0800

    1.2

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,18 +1,50 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
 int main()
 {
-  int n,count;
-  string str;
-  cin >> n;
-  while (n--)
-  {
-    cin >> str;
-    if (str[0] != str[1] && str[1] == str[2])
-    count++;
-  }
+    int len1 = 0,len2 = 0,num = 0;
+    int p1 = 1;
+    string s,t;
+    cin >> s >> t;
 
-  cout << count;
-  return 0;
+    len1 = t.size();
+    len2 = s.size();
+
+    for (int i = len1;i > 0;i--)
+    {
+      for (int j = 0;j < i;j++)
+      {
+        while(p1)
+        {
+          if (s[p1-1] == t[j])
+          {
+            p1++;
+            num++;
+            break;
+          }
+          
+          if (p1 > len2)
+          {
+          	p1 = 1;
+          	break;
+		  }
+          p1++;
+        }
+      }
+      if (num == i)
+      {
+        cout << num;
+        break;
+      }
+      else
+      {
+        num = 0;
+      }
+    }
+    
+	
+	
+	return 0;
 }
