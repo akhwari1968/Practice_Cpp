d310f7a194224d591d8d30e5b59cd1838484747a
king176769 <2630176769@qq.com>
2025-01-03 22:15:19 +0800

    1.3

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int len1 = 0,len2 = 0,num = 0;
+    int len1 = 0,len2 = 0,num = 0,tu = 0;
     int p1 = 1;
     string s,t;
     cin >> s >> t;
@@ -14,23 +14,30 @@ int main()
 
     for (int i = len1;i > 0;i--)
     {
+      tu = 0;
       for (int j = 0;j < i;j++)
       {
         while(p1)
         {
+          if (p1 > len2+1)
+          {
+          	p1 = 1;
+            t = 1;
+          	break;
+		      }
+
           if (s[p1-1] == t[j])
           {
             p1++;
             num++;
             break;
           }
-          
-          if (p1 > len2)
-          {
-          	p1 = 1;
-          	break;
-		  }
           p1++;
+        }
+        if (tu)
+        {
+          num = 0;
+          break;
         }
       }
       if (num == i)
@@ -38,13 +45,7 @@ int main()
         cout << num;
         break;
       }
-      else
-      {
-        num = 0;
-      }
     }
-    
-	
-	
+
 	return 0;
 }
