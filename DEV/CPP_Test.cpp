9fc8d229c98479300b7086fb24ee4aa9661d523b
king176769 <2630176769@qq.com>
2025-01-11 23:08:43 +0800

    1.11

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,1 +1,23 @@
-划水
+#include <iostream>
+#include <string>
+#include <ctype.h>
+using namespace std;
+int main()
+{
+	int num = 0;
+  string str1,str2;
+  cin >> str1 >> str2;
+
+  for (int i = 0;i < str2.size();i++)
+  {
+    if (isdigit(str2[i]))
+    {
+      num = str2[i] - '0';
+      str2[i] = str1[num];
+    }
+  }
+
+  cout << str2;
+
+  return 0;
+}
