f83a12bd6a10269bb211f33d1d53548ca1f4895b
king176769 <2630176769@qq.com>
2025-01-14 22:15:24 +0800

    1.14

--- DEV/字符串反转.cpp
+++ DEV/字符串反转.cpp
@@ -1,11 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 typedef long long ll;
 
 int main()
 {
-	
+	string str1;
+	getline(cin,str1);
+
+	for (int i = 0;i < str1.length() / 2;i++)
+	swap(str1[i],str1[str1.length() - i - 1]);
+	cout << str1 << '\n';
 	
 	return 0;
 }
