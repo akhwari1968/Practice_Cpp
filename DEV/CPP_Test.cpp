f95cefc704b2bdbed4116774e6d1b544aeb2629e
king176769 <2630176769@qq.com>
2025-01-15 22:28:17 +0800

    1.15

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,51 +1,71 @@
+/*#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+const int n = 5e5 + 3;
+int a[n];
+
+int main()
+{
+	
+	
+	int v;cin >> v;
+	for (int i = 1;i <= v;i++)cin >> a[i];
+	
+	sort(a+1,a+1+v);
+	
+	for (int i = 1;i <= v;i++)cout << a[i] << " \n"[i == v];
+	
+	sort(a+1,a+1+v,[](const int &u,const int &v)
+	{
+		return u < v;
+	});
+	
+	for (int i = v;i >= 1;i--)cout << a[i] << " \n"[i == 1];
+	
+	return 0;
+}*/
+
+
+//#include <bits/stdc++.h>
+//using namespace std;
+//typedef long long ll;
+//
+//const int N = 1e4 + 9;
+//int a[N];
+//
+//int main()
+//{
+//	//ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0)
+//	ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//	
+//	int n;cin >> n;
+//	
+//	for(int i = 1;i <= n;i++)cin >> a[i];
+//	cout << *max_element(a+1,a+1+n) << '\n';
+//	cout << *min_element(a+1,a+1+n) << '\n';
+//	
+//	ll sum = 0;
+//	for(int i = 1;i <= n;i++)sum += a[i];
+//	
+//	cout << fixed << setprecision(2) << 1.0 * sum / 2;
+//	
+//	return 0;
+//}
+
 #include <bits/stdc++.h>
 using namespace std;
-
 typedef long long ll;
 
 int main()
 {
-	/*string str1 = "hello";
-	getline(cin,str1);
+	ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
 	
-	/*string str2 = str1;
-	string str3 = str2.substr(0,4);
-	const char* chararr = "hello";
-	string str4(chararr);
-	string str5(5,'W');
+	int data[200];
+	for (int i = 0;i < 200;i++)data[i] = 4 * i + 6;
 	
-	cout << str1 << ' ' << str2 << ' ' << str3 << ' ' << str4 << ' ' << str5 << ' ' << chararr;*/
-	//cout << str1;
-	
-	/*char arr[100];
-	scanf("%s",arr);
-	string str(arr);
-	printf("str = %s\n",str.c_str());*/
-	
-	string str1 = "hello,world!";
-	/*string str2 = "hahahah";
-	string str3 = str1 + ',' + str2;
-	cout << str3;
-	
-	int a = str1.find(" ");
-	cout << a;
-	str1.replace(7,5,"wuyicong");
-	cout << "\n" << str1;*/
-	
-	/*string t = str1.substr(7,5);
-	cout << t << "\n";
-	string a = "aaa";
-	string b = "bbb";
-	if (a < b)
-	cout <<1;
-	else
-	cout << 2;*/
-	for (auto &i : str1)
-	{
-		cout << i;
-		i = '1';
-	}
-	cout << "\n" << str1;
+	int a;cin >> a;
+	cout << (lower_bound(data,data + 200,a) - data) << '\n';
 	
 	return 0;
 }
