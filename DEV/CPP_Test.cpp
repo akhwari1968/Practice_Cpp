9952e1b8e6077c45a550ef7429cb90a9f8d93d09
king176769 <2630176769@qq.com>
2025-01-16 22:01:30 +0800

    1.16

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,54 +1,40 @@
-/*#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-
-const int n = 5e5 + 3;
-int a[n];
-
-int main()
-{
-	
-	
-	int v;cin >> v;
-	for (int i = 1;i <= v;i++)cin >> a[i];
-	
-	sort(a+1,a+1+v);
-	
-	for (int i = 1;i <= v;i++)cout << a[i] << " \n"[i == v];
-	
-	sort(a+1,a+1+v,[](const int &u,const int &v)
-	{
-		return u < v;
-	});
-	
-	for (int i = v;i >= 1;i--)cout << a[i] << " \n"[i == 1];
-	
-	return 0;
-}*/
-
-
 //#include <bits/stdc++.h>
 //using namespace std;
 //typedef long long ll;
 //
-//const int N = 1e4 + 9;
-//int a[N];
-//
 //int main()
 //{
-//	//ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0)
 //	ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
 //	
-//	int n;cin >> n;
+//	vector<int> nums = {3,2,1};
 //	
-//	for(int i = 1;i <= n;i++)cin >> a[i];
-//	cout << *max_element(a+1,a+1+n) << '\n';
-//	cout << *min_element(a+1,a+1+n) << '\n';
+//	cout << "初始:";
+//	for(auto num : nums)
+//	{
+//		cout << num << "";
+//	}
+//	cout << endl;
 //	
-//	ll sum = 0;
-//	for(int i = 1;i <= n;i++)sum += a[i];
+////	while(next_permutation(nums.begin(),nums.end()))
+////	{
+////		cout << "下一序列: ";
+////		for(auto num : nums)
+////		{
+////			cout << num << "";
+////		}
+////		cout << endl;
+////	}
+//
+//	while(prev_permutation(nums.begin(),nums.end()))
+//	{
+//		cout << "序列为：";
+//		for (auto num : nums)
+//		{
+//			cout << num << " ";
+//		}
+//		cout << endl;
+//	}
 //	
-//	cout << fixed << setprecision(2) << 1.0 * sum / 2;
 //	
 //	return 0;
 //}
@@ -57,15 +43,23 @@ int main()
 using namespace std;
 typedef long long ll;
 
+int a[10];
+
 int main()
 {
 	ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	for (int i = 1;i <= 4;i++)
+	a[i] = i;
 	
-	int data[200];
-	for (int i = 0;i < 200;i++)data[i] = 4 * i + 6;
+	bool tag = true;
 	
-	int a;cin >> a;
-	cout << (lower_bound(data,data + 200,a) - data) << '\n';
+	while(tag)
+	{
+		for(int i = 1;i <= 4;i++)
+		cout << a[i] << ' ';
+		cout << '\n';
+		tag = next_permutation(a+1,a+1+4);
+	}
 	
 	return 0;
 }
