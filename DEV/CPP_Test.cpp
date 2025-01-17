f4928709f1b137d92ba04dccb4cae546e8bc5de0
king176769 <2630176769@qq.com>
2025-01-17 22:52:14 +0800

    1.17

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,65 +1,13 @@
-//#include <bits/stdc++.h>
-//using namespace std;
-//typedef long long ll;
-//
-//int main()
-//{
-//	ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
-//	
-//	vector<int> nums = {3,2,1};
-//	
-//	cout << "初始:";
-//	for(auto num : nums)
-//	{
-//		cout << num << "";
-//	}
-//	cout << endl;
-//	
-////	while(next_permutation(nums.begin(),nums.end()))
-////	{
-////		cout << "下一序列: ";
-////		for(auto num : nums)
-////		{
-////			cout << num << "";
-////		}
-////		cout << endl;
-////	}
-//
-//	while(prev_permutation(nums.begin(),nums.end()))
-//	{
-//		cout << "序列为：";
-//		for (auto num : nums)
-//		{
-//			cout << num << " ";
-//		}
-//		cout << endl;
-//	}
-//	
-//	
-//	return 0;
-//}
-
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
-int a[10];
-
 int main()
 {
 	ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
-	for (int i = 1;i <= 4;i++)
-	a[i] = i;
 	
-	bool tag = true;
-	
-	while(tag)
-	{
-		for(int i = 1;i <= 4;i++)
-		cout << a[i] << ' ';
-		cout << '\n';
-		tag = next_permutation(a+1,a+1+4);
-	}
+	int 年龄  = 18;
+	cout << 年龄;
 	
 	return 0;
 }
--- VS/C++02/C++02/study02.cpp
+++ VS/C++02/C++02/study02.cpp
@@ -1,110 +1,13 @@
-#define  _CRT_SECURE_NO_WARNINGS 1
 #include <iostream>
-#include <iomanip>//输入输出格式库
-#include <string>
-#define pi 3.1415926
-//#define Swap(num1,num2) (temp = num1,num1 = num2,num2 = temp)
 using namespace std;
+typedef long long ll;
 
-void show(const int& v)
-{
-	
-}
-
-int isprime(int m)
-{
-	int i = 0;
-	int t = 1;
-	for (i = 2; i < m; i++)
-	{
-		if (!(m % i))
-		{
-			t = 0;
-			return t;
-		}
-	}
-	return t;
-}
-
-//int temp = 0;
 int main()
 {
-	/*int val1 = 0;
-	int val2 = 0;
-	cin >> val1 >> val2;
-	Swap(val1, val2);
-	cout << val1 << " " << val2 << endl;*/
+	string 时间;
+	cin >> 时间;
+	int 年龄 = 18;
+	cout << 年龄 << endl << 时间;
 
-	/*string str;
-	getline(cin, str);
-	int i = 0;
-	for (i = 0; i < str.length(); i++)
-	{
-		if (isalpha(str[i]))
-		{
-			if (str[i] == 122)
-			{
-				str[i] = 'a';
-			}
-			else
-			{
-				str[i] += 1;
-			}
-		}
-	}
-
-	cout << str << endl;*/
-
-	/*string str;
-	getline(cin, str);
-	int len = str.length();
-	int i = 0;
-	for (i = len - 1; i >= 0; i--)
-	{
-		printf("%c", str[i]);
-	}
-	cout << endl;*/
-
-	/*int m = 0, n = 0,sum = 0;
-	cin >> m >> n;
-	for (m = m; m <= n; m++)
-	{
-		if (isprime(m))
-		{
-			sum += m;
-		}
-	}
-
-	cout << sum << endl;*/
-
-	/*int arr[10];
-	int i = 0;
-	int sum = 0, count = 0;
-	for (i = 0; i < 10; i++)
-	{
-		cin >> arr[i];
-		sum += arr[i];
-	}
-
-	sum /= 10;
-	for (i = 0; i < 10; i++)
-	{
-		if (arr[i] > sum)
-		{
-			count++;
-		}
-	}
-
-	cout << count << endl;*/
-
-	//double r = 0.0;
-	//cin >> r;
-	//cout << fixed << setprecision(2) << pi * r * r << endl;//fixed设置为定点输出模式，setprecision设置小数点后几位
-
-	int a = 10;
-	show(a);
-	a = 100;
-
-	system("pause");
 	return 0;
 }
