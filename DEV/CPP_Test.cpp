4f214df48964da99be24558c8f9ab32ea9b7954f
king176769 <2630176769@qq.com>
2025-01-28 23:25:47 +0800

    1.28

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,1 +1,12 @@
-划水.......
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+	ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	
+	cout << "hello little snake";
+	
+	return 0;
+}
