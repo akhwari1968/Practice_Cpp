e7521e9af2ca9848b9ef00a3301147bff0ad5093
king176769 <2630176769@qq.com>
2025-01-29 23:07:33 +0800

    1.29

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -6,7 +6,11 @@ int main()
 {
 	ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
 	
-	cout << "hello little snake";
+	vector<int> vec = {10,20,30};
+	for (auto it = vec.begin(); it != vec.end(); it++)
+	{
+		cout << *it << " ";
+	}
 	
 	return 0;
 }
