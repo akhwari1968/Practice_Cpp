59bcd52646bc5e3fc289588ed23472c55f9bcb32
king176769 <2630176769@qq.com>
2025-02-14 23:32:29 +0800

    2.13

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,4 +1,4 @@
-#include <bits/stdc++.h>
+#include  <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
 
@@ -6,7 +6,7 @@ int main()
 {
 	ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
 	
-	
+	..
 	
 	return 0;
 }
