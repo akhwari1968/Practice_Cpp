e9179a000fc47f707c1a987aa3ad055f5bbba7cb
king176769 <2630176769@qq.com>
2025-02-26 21:51:47 +0800

    2.26

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,26 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
 int main()
 {
-	int i = 0;
-  string str1;
-  cin >> str1;
-
-  sort(str1.begin(),str1.end());
-
-  while(i < (int)str1.size())
-  {
-    if (str1[i] == str1[i+1])
-    {
-      i += 2;
-    }
-    else
-    break;
-  }
-  if(i < str1.size())
-  cout << "NO";
-  else
-  cout << "YES";
-
-  return 0;
+	ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	
+	
+	
+	return 0;
 }
