1befe1068f9e916b2e0af2e8feb407aa306a9d43
king176769 <2630176769@qq.com>
2025-03-03 22:04:40 +0800

    3.3

--- DEV/链表合并.c
+++ DEV/链表合并.c
@@ -22,23 +22,67 @@ int insertlist(LNode *L,int x,int y)
 	S->b = y;
 	S->next = L->next;
 	L->next = S;
+	return 1;
 }
 
 int main()
 {
 	int n,m,x,y;
-	LinkList L1;
+	LinkList L1,L2,L3;
 	initlist(&L1);
 	scanf("%d" "%d",&n,&m);
-	for (int i = n;i > 0;i--)
+	for (int i = 0;i < n;i++)
 	{
 		scanf("%d %d",&x,&y);
 		insertlist(L1,x,y);
 	}
-	for (int i = m;i  > 0;i--)
+	
+	initlist(&L2);
+	for (int i = 0;i < m;i++)
 	{
-		
+		scanf("%d %d",&x,&y);
+		insertlist(L2,x,y);
 	}
 	
+	initlist(&L3);
+	LNode *p1 = L1->next, *p2 = L2->next, *p3 = L3;
+	while(p1 != NULL && p2 != NULL)
+	{
+		if (p1->next->a < p2->next->a)
+		{
+			insertlist(p3,p1->next->a,p1->next->b);
+			p1 = p1->next;
+		}
+		else
+		{
+			insertlist(p3,p2->next->a,p2->next->b);
+			p2 = p2->next;
+		}
+	}
+	
+	if (p1 == NULL)
+	{
+		while(p2 != NULL)
+		{
+			insertlist(p3,p2->next->a,p2->next->b);
+			p2 = p2->next;
+		}
+	}
+	else if (p2 == NULL)
+	{
+		while(p1 != NULL)
+		{
+			insertlist(p3,p1->next->a,p1->next->b);
+			p1 = p1->next;
+		}
+	}
+	
+	while(p3 != NULL)
+	{
+		printf("%d %d",p3->next->a,p3->next->b);
+		printf("\n");
+		p3 = p3->next;
+	}
+//printf("%d %d",L1->next->a,L2->next->a);
 	return 0;
 }
