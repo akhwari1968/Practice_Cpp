fb099da3914a8f57229a7baa0f1fdcd7e458c9f2
king176769 <2630176769@qq.com>
2025-03-04 21:34:17 +0800

    3.4

--- DEV/链表合并.c
+++ DEV/链表合并.c
@@ -43,46 +43,21 @@ int main()
 		scanf("%d %d",&x,&y);
 		insertlist(L2,x,y);
 	}
+	printf("\n");
 	
 	initlist(&L3);
-	LNode *p1 = L1->next, *p2 = L2->next, *p3 = L3;
-	while(p1 != NULL && p2 != NULL)
+	LNode *p1 = L1, *p2 = L2, *p3 = L3;
+	while()
 	{
-		if (p1->next->a < p2->next->a)
-		{
-			insertlist(p3,p1->next->a,p1->next->b);
-			p1 = p1->next;
-		}
-		else
-		{
-			insertlist(p3,p2->next->a,p2->next->b);
-			p2 = p2->next;
-		}
+		
 	}
 	
-	if (p1 == NULL)
-	{
-		while(p2 != NULL)
-		{
-			insertlist(p3,p2->next->a,p2->next->b);
-			p2 = p2->next;
-		}
-	}
-	else if (p2 == NULL)
-	{
-		while(p1 != NULL)
-		{
-			insertlist(p3,p1->next->a,p1->next->b);
-			p1 = p1->next;
-		}
-	}
-	
-	while(p3 != NULL)
+	while(p3->next != NULL)
 	{
 		printf("%d %d",p3->next->a,p3->next->b);
 		printf("\n");
 		p3 = p3->next;
 	}
-//printf("%d %d",L1->next->a,L2->next->a);
+
 	return 0;
 }
