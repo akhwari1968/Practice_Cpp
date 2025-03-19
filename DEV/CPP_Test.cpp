39745000c9041db83e9bc86d6775a1acdbb87010
king176769 <2630176769@qq.com>
2025-03-19 22:37:15 +0800

    3.19

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    int sum=0;
-    for(int i=7;i<=49;i++) sum+=i;
-    cout<<sum;
-    return 0;
+typedef long long ll;
+
+int main()
+{
+	ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	
+	return 0;
 }
