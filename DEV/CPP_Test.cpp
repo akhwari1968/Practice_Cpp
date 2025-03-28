6fe5393a4f3247fd9d905e4fa0400934b91f1caa
king176769 <2630176769@qq.com>
2025-03-28 22:35:38 +0800

    3.28

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -85,7 +85,7 @@ int main()
 	}
 	cout << i-1;*/
 	
-	int n = 0,x = 2024;
+	/*int n = 0,x = 2024;
 	
 	while(x)
 	{
@@ -96,7 +96,33 @@ int main()
 		x = x >> 1;
 	}
 	
-	cout << n;
+	cout << n;*/
+	
+	ll n = 0,k = 0,tmp = 0;
+	ll i = 0,j = 0,t = 0;
+	vector<int> arr;
+	cin >> n >> k;
+	for (i = 0;i < n;i++)
+	{
+		cin >> tmp;
+		arr.push_back(tmp);
+	}
+	sort(arr.begin(),arr.end(),[](int a,int b)
+	{
+		return a > b;
+	});
+	
+	for (i;i < arr.size();i++)
+	{
+		for (j = i+1;j < arr.size();j++)
+		{
+			for (t = j+1;t < arr.size();t++)
+			{
+				
+			}
+		}
+	}
+	
 	
 	return 0;
 }
