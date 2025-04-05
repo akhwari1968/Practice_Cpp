913059f9b6a7f05e09e0c0efbf9b41479ea8292c
king176769 <2630176769@qq.com>
2025-04-05 22:49:52 +0800

    4.5

--- DEV/数位排序.cpp
+++ DEV/数位排序.cpp
@@ -2,12 +2,41 @@
 using namespace std;
 typedef long long ll;
 
+int nums(int j)
+{
+	int sum = 0;
+	while(j)
+	{
+		sum += j % 10;
+		j /= 10;
+	}
+	
+	return sum;
+}
+
 int main()
 {
 	ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
 	
+	multimap<int,int> mmap;
 	int n = 0,m = 0;
 	cin >> n >> m;
+	
+	for (int i = 1;i <= n;i++)
+	{
+		int sum = nums(i);
+		mmap.insert(make_pair(sum,i));
+	}
+	sort(v.begin(),)
+	int count = 0;
+	for (auto it = mmap.begin();it != mmap.end();it++)
+	{
+		count++;
+		if (count == m)
+		{
+			cout <<  it->second;
+		}
+	}
 	
 	return 0;
 }
