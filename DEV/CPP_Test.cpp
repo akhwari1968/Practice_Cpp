2e3ed44ff074a777625d77eab4b82628f985270b
king176769 <2630176769@qq.com>
2025-04-08 23:00:40 +0800

    4.8

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,36 +1,55 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
 
-int main()
-{
-	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-	
-	int i = 0,j = 0;
-	cin >> i;
-	vector<int> v;
-	
-	for (int n = 0;n < i;n++)
-	{
-		while (cin >> j)
-		{
-			v.push_back(j);
-			if (cin.get() == '\n')
-			break;
-		}
-	}
-	
-	int num1 = 0,num2 = 0;
-	sort(v.begin(),v.end());
-	for (i = 1;i < v.size();i++)
-	{
-		if (v[i] == v[i+1])
-		num1 = v[i];
-		if (v[i+1] - v[i] == 2)
-		num2 = v[i] + 1;
-	}
-	
-	cout << num2 << ' ' << num1;
-	
-	return 0;
-}
+//#include <bits/stdc++.h>
+//using namespace std;
+//typedef long long ll;
+//
+//int main()
+//{
+//  ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
+//
+//  int n = 0,k = 0,max = 0,sum = 0;
+//  vector <int> v1;
+//  vector <int> v2;
+//  vector <int> v3;
+//	cin >> n >> k;
+//  for (int i = 1;i <= n;i++)
+//  {
+//    int j = 0,l = 0;
+//    cin >> j >> l;
+//    v1.push_back(j);
+//    v2.push_back(l);
+//    v3.push_back(i);
+//	}
+//	int len = v1.size();
+//  
+//  int left = 0,right = n-1;
+//  while(left < right)
+//  {
+//	int mid = (left + right) / 2;
+//
+//	for (int i = 0;i < len;i++)
+//	{
+//		if (v1[i] >= v2[i])
+//		{
+//			sum += v1[i] / mid;
+//		}
+//		else
+//		{
+//			sum += v2[i] / mid;
+//		}
+//	}
+//	if (sum >= k)
+//	{
+//		max = mid;
+//		left = mid +1;
+//	}
+//	else
+//	right = mid -1;
+//	sum = 0;
+//	}
+//	
+//  
+//  cout << max;
+//  
+//  return 0;
+//}
