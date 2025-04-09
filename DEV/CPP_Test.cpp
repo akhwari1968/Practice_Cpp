3a507847dfa95c6b94ab797c29b052c336dac11e
king176769 <2630176769@qq.com>
2025-04-09 22:48:58 +0800

    4.9

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,55 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
 
-//#include <bits/stdc++.h>
-//using namespace std;
-//typedef long long ll;
-//
-//int main()
-//{
-//  ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
-//
-//  int n = 0,k = 0,max = 0,sum = 0;
-//  vector <int> v1;
-//  vector <int> v2;
-//  vector <int> v3;
-//	cin >> n >> k;
-//  for (int i = 1;i <= n;i++)
-//  {
-//    int j = 0,l = 0;
-//    cin >> j >> l;
-//    v1.push_back(j);
-//    v2.push_back(l);
-//    v3.push_back(i);
-//	}
-//	int len = v1.size();
-//  
-//  int left = 0,right = n-1;
-//  while(left < right)
-//  {
-//	int mid = (left + right) / 2;
-//
-//	for (int i = 0;i < len;i++)
-//	{
-//		if (v1[i] >= v2[i])
-//		{
-//			sum += v1[i] / mid;
-//		}
-//		else
-//		{
-//			sum += v2[i] / mid;
-//		}
-//	}
-//	if (sum >= k)
-//	{
-//		max = mid;
-//		left = mid +1;
-//	}
-//	else
-//	right = mid -1;
-//	sum = 0;
-//	}
-//	
-//  
-//  cout << max;
-//  
-//  return 0;
-//}
+const int d = 100005;
+
+int main()
+{
+	ios :: sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	
+	vector<int> v1;
+	vector<int> v2;
+	int n = 0,k = 0,sum = 0,maxv = 0;
+	cin >> n >> k;
+	
+	int i = 0;
+	for (i = 0;i < n;i++)
+	{
+		int a = 0,b = 0;
+		cin >> a >> b;
+		v1.push_back(a);
+		v2.push_back(b);
+	}
+	
+	int left = 0,right = d;
+	while(left < right)
+	{
+		sum = 0;
+		int mid = (left + (right-left)) / 2;
+		for (i = 0;i < n;i++)
+		{
+			sum += (v1[i]/mid)*(v2[i]/mid);
+		}
+		if (sum >= k)
+		{
+			maxv = mid;
+			left = mid + 1;
+		}
+		else
+		right = mid;
+	}
+	
+	cout << maxv;
+	
+	return 0;
+}
