083a5de9182f53fe09f0db3f98ecb1a49f5b45ae
king176769 <2630176769@qq.com>
2025-07-05 21:47:55 +0800

    重新开始

--- DEV/CPP_Test.cpp
+++ DEV/CPP_Test.cpp
@@ -1,24 +1,11 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    vector<int> tails;
-    for (int num : a) {
-        auto it = lower_bound(tails.begin(), tails.end(), num);
-        if (it == tails.begin()) {
-            tails.insert(tails.begin(), num);
-        } else {
-            *(it - 1) = num;
-        }
-    }
-    cout << tails.size() << endl;
-    return 0;
+int main()
+{
+	ios :: sync_with_stdio(),cin.tie(0),cout.tie(0);
+	
+	
+	return 0;
 }
